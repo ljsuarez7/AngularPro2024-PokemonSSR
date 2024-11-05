4befabf1c7864eb22ec5f995f400b456a0a0bea9
JaviSrz <ljsuarez7@gmail.com>
2024-11-05 21:48:27 +0100

    añadido active a los enlaces de la navbar

--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
@@ -12,8 +13,15 @@ export default class PricingPageComponent implements OnInit {
 
   private title = inject(Title);
   private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
 
   ngOnInit(): void {
+
+    // if (isPlatformServer(this.platform)) {
+    // if (isPlatformBrowser(this.platform)) {
+    //   document.title = 'Pricing page'
+    // }
+
     this.title.setTitle('Pricing Page');
     this.meta.updateTag({ name: 'description', content: 'Este es mi Pricing page' });
     this.meta.updateTag({ name: 'og:title', content: 'Pricing Page' });
