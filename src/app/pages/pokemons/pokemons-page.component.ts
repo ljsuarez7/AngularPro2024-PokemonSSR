a4988a8c32d2407b0bd9798a92eb46e9dfe2da35
JaviSrz <ljsuarez7@gmail.com>
2024-11-06 23:07:40 +0100

    pagina de lista de pokemon y componentes base

---
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -0,0 +1,32 @@
+import { ApplicationRef, ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
+import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
+
+@Component({
+  selector: 'pokemons-page',
+  standalone: true,
+  imports: [PokemonListComponent, PokemonListSkeletonComponent],
+  templateUrl: './pokemons-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class PokemonsPageComponent implements OnInit {
+
+  // public isLoading = signal(true);
+
+  // private appRef = inject(ApplicationRef);
+
+  // private $appState = this.appRef.isStable.subscribe(isStable => {
+  //   console.log({ isStable });
+  // });
+
+  ngOnInit(): void {
+    // setTimeout(() => {
+    //   this.isLoading.set(false);
+    // }, 5000);
+  }
+
+  // ngOnDestroy(): void {
+  //   this.$appState.unsubscribe();
+  // }
+
+}
