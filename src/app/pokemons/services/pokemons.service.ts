db265ba56f63437505eb09f884f9872087298398
JaviSrz <ljsuarez7@gmail.com>
2024-11-07 19:51:53 +0100

    servicio para llamar a la api de pokemon

---
+++ src/app/pokemons/services/pokemons.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
+import { PokeAPIResponse, SimplePokemon } from '../interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonsService {
+
+  private http = inject(HttpClient);
+
+  public loadPage(page: number): Observable<SimplePokemon[]> {
+
+    if (page !== 0) {
+      --page;
+    }
+
+    page = Math.max(0, page);
+
+    return this.http.get<PokeAPIResponse>(
+      `https://pokeapi.co/api/v2/pokemon?offset=${page * 20}&limit=20`
+    ).pipe(
+      map(resp => {
+        const simplePokemon: SimplePokemon[] = resp.results.map(pokemon => ({
+          id: pokemon.url.split('/').at(-2) ?? '',
+          name: pokemon.name
+        }))
+        return simplePokemon;
+      })
+    );
+
+  }
+
+}
